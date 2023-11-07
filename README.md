b94e5de920b963df98e45ada565ee1e13d5eea37
meenbeese <meenbeese@tutanota.com>
2023-11-07 18:41:57 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# quarkus-web-app
- A Java web app built with Quarkus + JPAStreamer
--- src/main/java/org/freecodecamp/app/model/Film.java
+++ src/main/java/org/freecodecamp/app/model/Film.java
@@ -1,4 +1,4 @@
-package com.meenbeese.app.model;
+package org.freecodecamp.app.model;
 
 import jakarta.persistence.*;
 
@@ -11,64 +11,52 @@ import java.util.Objects;
 @Entity
 @Table(name = "film", schema = "sakila")
 public class Film {
-
+    
     public Film() {}
-
+    
     public Film(short filmId, String title, short length) {
-        this.filmId = filmId;
-        this.title = title;
+        this.filmId = filmId; 
+        this.title = title; 
         this.length = length;
     }
-
+    
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "film_id")
     private short filmId;
-
     @Basic
     @Column(name = "title")
     private String title;
-
     @Basic
     @Column(name = "description")
     private String description;
-
     @Basic
     @Column(name = "language_id")
     private short languageId;
-
     @Basic
     @Column(name = "original_language_id")
     private Short originalLanguageId;
-
     @Basic
     @Column(name = "rental_duration")
     private short rentalDuration;
-
     @Basic
     @Column(name = "rental_rate", columnDefinition = "decimal(4,2)")
     private Float rentalRate;
-
     @Basic
     @Column(name = "length")
     private Short length;
-
     @Basic
     @Column(name = "replacement_cost")
     private BigDecimal replacementCost;
-
     @Basic
     @Column(name = "rating", columnDefinition = "enum('G', 'PG', 'PG-13', 'R', 'NC-17')")
     private String rating;
-
     @Basic
     @Column(name = "special_features", columnDefinition = "set('Trailers', 'Commentaries', 'Deleted Scenes', 'Behind the Scenes')")
     private String specialFeatures;
-
     @Basic
     @Column(name = "last_update")
     private Timestamp lastUpdate;
-
     @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
     @JoinTable(
             name = "film_actor",
@@ -100,7 +88,7 @@ public class Film {
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public short getLanguageId() {
         return languageId;
     }
@@ -178,23 +166,12 @@ public class Film {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Film film = (Film) o;
-        return filmId == film.filmId &&
-                languageId == film.languageId &&
-                rentalDuration == film.rentalDuration &&
-                Objects.equals(title, film.title) &&
-                Objects.equals(description, film.description) &&
-                Objects.equals(originalLanguageId, film.originalLanguageId) &&
-                Objects.equals(rentalRate, film.rentalRate) &&
-                Objects.equals(length, film.length) &&
-                Objects.equals(replacementCost, film.replacementCost) &&
-                Objects.equals(rating, film.rating) &&
-                Objects.equals(specialFeatures, film.specialFeatures) &&
-                Objects.equals(lastUpdate, film.lastUpdate);
+        return filmId == film.filmId && languageId == film.languageId && rentalDuration == film.rentalDuration && Objects.equals(title, film.title) && Objects.equals(description, film.description) && Objects.equals(originalLanguageId, film.originalLanguageId) && Objects.equals(rentalRate, film.rentalRate) && Objects.equals(length, film.length) && Objects.equals(replacementCost, film.replacementCost) && Objects.equals(rating, film.rating) && Objects.equals(specialFeatures, film.specialFeatures) && Objects.equals(lastUpdate, film.lastUpdate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(filmId, title, description, languageId,originalLanguageId, rentalDuration, rentalRate, length, replacementCost, rating, specialFeatures, lastUpdate);
+        return Objects.hash(filmId, title, description, languageId, originalLanguageId, rentalDuration, rentalRate, length, replacementCost, rating, specialFeatures, lastUpdate);
     }
 
     public List<Actor> getActors() {
